1a68a1da3e6df0ae8c558f8e5411847bc4735a14
90 <hi@90.gripe>
2023-10-15 22:34:47 +0100

    Only suppress flow to a BlockPos within range and not elsewhere

--- common/src/main/java/gripe/_90/hydrophobe/mixin/FlowingFluidMixin.java
+++ common/src/main/java/gripe/_90/hydrophobe/mixin/FlowingFluidMixin.java
@@ -4,8 +4,10 @@ import gripe._90.hydrophobe.Hydrophobe;
 import gripe._90.hydrophobe.HydrophobeBlock;
 import java.util.stream.Stream;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.Vec3i;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FlowingFluid;
 import net.minecraft.world.level.material.FluidState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,25 +18,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(FlowingFluid.class)
 public class FlowingFluidMixin {
-    @Inject(method = "spread", at = @At("HEAD"), cancellable = true)
-    private void suppressFlow(Level level, BlockPos pos, FluidState state, CallbackInfo ci) {
-        if (state.is(Hydrophobe.HYDROPHOBE_TAG)) {
-            hydrophobe$suppress(level, pos, state, ci, Hydrophobe.HYDROPHOBE_RANGE);
-        } else if (state.is(Hydrophobe.MAGMAPHOBE_TAG)) {
-            hydrophobe$suppress(level, pos, state, ci, Hydrophobe.MAGMAPHOBE_RANGE);
+    @Inject(method = "spreadTo", at = @At("HEAD"), cancellable = true)
+    private void suppressFlow(
+            LevelAccessor level,
+            BlockPos pos,
+            BlockState blockState,
+            Direction direction,
+            FluidState fluidState,
+            CallbackInfo ci) {
+        if (fluidState.is(Hydrophobe.HYDROPHOBE_TAG)) {
+            hydrophobe$suppress(level, pos, fluidState, ci, Hydrophobe.HYDROPHOBE_RANGE);
+        } else if (fluidState.is(Hydrophobe.MAGMAPHOBE_TAG)) {
+            hydrophobe$suppress(level, pos, fluidState, ci, Hydrophobe.MAGMAPHOBE_RANGE);
         }
     }
 
     @Unique
-    private void hydrophobe$suppress(Level level, BlockPos pos, FluidState state, CallbackInfo ci, int range) {
-        var bound = range + 1;
-        var p1 = pos.subtract(new Vec3i(bound, bound, bound));
-        var p2 = pos.subtract(new Vec3i(-bound, -bound, -bound));
+    private void hydrophobe$suppress(LevelAccessor level, BlockPos pos, FluidState state, CallbackInfo ci, int range) {
+        var p1 = pos.subtract(new Vec3i(range, range, range));
+        var p2 = pos.subtract(new Vec3i(-range, -range, -range));
 
         BlockPos.betweenClosedStream(p1, p2)
                 .filter(p -> Stream.of(pos.getX() - p.getX(), pos.getY() - p.getY(), pos.getZ() - p.getZ())
                         .map(Math::abs)
-                        .anyMatch(i -> i == bound))
+                        .anyMatch(i -> i == range))
                 .forEach(p -> {
                     if (level.getBlockState(p).getBlock() instanceof HydrophobeBlock hydrophobe) {
                         if (state.is(hydrophobe.getFluidTag())) {
